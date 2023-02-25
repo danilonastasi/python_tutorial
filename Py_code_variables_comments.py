2e399e437ebdca6ba36a7a0219528972f32addba
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2023-02-25 15:38:01 +0100

    Create Py_code_variables_comments.py

---
+++ Py_code_variables_comments.py
@@ -0,0 +1,22 @@
+# Variables and comments in Python
+
+numero = 10
+stringa = "Python"
+lista = [1, 2, 3]
+# as you can see we don't need to declare variables
+
+x = 10
+x = 20
+# the value '10' will be deleted
+
+x = 10
+x = "Python"
+x = [1, 2, 3]
+# the final association is the last one which is a list
+
+# let's make a multiple assignment
+a, b, c = 2, 3, 5 # multiple assignment
+a * b + c
+a
+b
+c
