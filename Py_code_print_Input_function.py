e91bd9db7a0bf28996552a1f37f030e8fe783c0f
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2023-02-25 15:13:20 +0100

    Create Py_code_print_Input_function.py

---
+++ Py_code_print_Input_function.py
@@ -0,0 +1,31 @@
+# Print and Input Function
+
+# assignment
+a = 12
+b = 3
+# let's print values
+print(a, b, a-b)
+
+c = "Hello"
+print(c, "World")
+
+x = 10
+x
+
+x * 2
+
+# let's assign a name to the object nome
+nome = input("Inerisci il tuo nome: ")
+nome
+
+raggio = input("Inserisci il raggio: ")
+raggio
+# it will be a string
+# let's convert string in value:
+r = float(raggio)
+raggio
+r
+
+# let's calculate and print area and circumference
+print("Area:", 3.14 * r**2)
+print("Circonferenza:", 2 * 3.14 * r)
