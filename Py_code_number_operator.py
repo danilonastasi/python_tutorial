673d8a04fef9ab468a8a9c019021db6f199eb05d
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2023-02-25 16:35:41 +0100

    Create Py_code_number_operator.py

---
+++ Py_code_number_operator.py
@@ -0,0 +1,54 @@
+6 + 4 * 3
+
+9 / 2 # result will be a float number
+
+int(10.6) # result will be 10, integer number
+
+float(20) # result will be float number 20.0
+
+0b11111111 # converfting from binary system, prefix is 0b
+
+0o377  # converting from ott system, prefix is 0o
+
+0xFF # converting from hex system, prefix is 0x
+
+9 // 4 # result will be 2, integer division
+
+9 % 4 # result will be 1, the rest of the division, modul
+
+8 == 8 # is it equal?
+
+3 == 5
+
+3 != 5 # is it different?
+
+8 != 8
+
+3 < 5 # is 3 less than 5? idem > bigger
+
+3 <= 5 # is 3 less or equal to 5? idem >= bigger/equal
+
+
+# let's type Booleans operator
+# AND  --> retur True if both operandi are true, if not return False
+# OR --> return True is at least one of the operandi is true, if not return False
+# NOT --> return False if operando is true, return True if operand is False
+
+
+# in Python every object is or True(numbers different from 0, costant True, containers with at least one element)
+# or False(it means the number 0, costant False and None, empty containers)
+
+# bool(object), bool is a function to verify if the object is True or False.
+
+# if the first Boolena operator (AND, OR or other) is the one can find the result, the second operator is not run.
+
+
+# Binary operators, bitwise
+
+x << n # run a shift to the left of n positions of the bit x
+x >> n # run a shift to the right of n positions of the bit x
+x & y # run a AND between bit x and bit y
+x | y # run OR between bit x and bit y
+x ^ y # run exclusive OR between bit x and bit y
+~x # invert bit of x
+
